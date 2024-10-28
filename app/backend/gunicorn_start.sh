437a0e5c966861e6bbc080babbb7c2ab14207ce1
ageller <a-geller@northwestern.edu>
2024-10-28 14:37:44 -0500

    updating gunicorn request size

--- app/backend/gunicorn_start.sh
+++ app/backend/gunicorn_start.sh
@@ -2,4 +2,4 @@
 
 source /home/www-worker/.bashrc
 
-gunicorn server:app --workers 3 --bind 0.0.0.0:5000 --reload
+gunicorn server:app -limit-request-field-size 8190 --limit-request-line 4094 --workers 1 --bind 0.0.0.0:5000 --reload
